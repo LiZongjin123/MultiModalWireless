6fedd984d8027433e680a9f70f3e000a8e512d98
weqweqe647 <2575634450@qq.com>
2026-02-04 00:28:56 +0800

    finished cav's yaml file but vehicles are incomplete

--- cav.py
+++ cav.py
@@ -1,11 +1,14 @@
+import math
 import os
 import struct
-
 import carla
 import queue
+import yaml
+
 
 class Cav:
     def __init__(self, vehicle, index, sensors_config, root_dir_path):
+        self.__index = index
         self.__vehicle = vehicle
         self.__blueprint_library = vehicle.get_world().get_blueprint_library()
         self.__world = vehicle.get_world()
@@ -64,6 +67,7 @@ class Cav:
     def __attach_sensors(self):
         self.__attach_cameras()
         self.__attach_lidar()
+        self.__attach_imu()
 
     def __attach_lidar(self):
         lidar_config = self.__sensors_config["cav"]["lidar"]
@@ -91,7 +95,7 @@ class Cav:
 
         imu_transform = carla.Transform(imu_location)
         imu_attributes = imu_config["attributes"]
-        for key, value in imu_attributes.item():
+        for key, value in imu_attributes.items():
             imu_blueprint.set_attribute(key, str(value))
 
         imu = self.__world.spawn_actor(imu_blueprint, imu_transform, attach_to=self.__vehicle)
@@ -131,6 +135,149 @@ class Cav:
         with open(pcd_file_path, 'w', encoding='utf-8') as f:
             f.write(pcd_content)
 
-    def save_data(self):
+    def save_data(self, vehicles):
         self.__save_image()
         self.__save_lidar_to_pcd()
+        self.__save_numeric_data_as_yaml(vehicles)
+
+    def __save_numeric_data_as_yaml(self, vehicles):
+        all_camera_data = {}
+        for i in range(0, 4):
+            camera_name = f"camera{i}"
+            camera_transform = self.__sensors[camera_name].get_transform()
+            camera_location = camera_transform.location
+            camera_rotation = camera_transform.rotation
+            camera_data = {
+                "location": {
+                    "x": camera_location.x,
+                    "y": camera_location.y,
+                    "z": camera_location.z
+                },
+                "rotation": {
+                    "pitch": camera_rotation.pitch,
+                    "roll": camera_rotation.roll,
+                    "yaw": camera_rotation.yaw
+                }
+            }
+            all_camera_data[camera_name] = camera_data
+
+        cav_speed = self.__vehicle.get_velocity()
+        cav_transform = self.__vehicle.get_transform()
+        cav_location = cav_transform.location
+        cav_rotation = cav_transform.rotation
+
+        lidar_transform = self.__sensors["lidar"].get_transform()
+        lidar_location = lidar_transform.location
+        lidar_rotation = lidar_transform.rotation
+
+        imu_measurement = self.__sensor_queues["imu"].get()
+        frame_id = imu_measurement.frame
+        accelerometer = imu_measurement.accelerometer
+        gyroscope = imu_measurement.gyroscope
+        compass = imu_measurement.compass
+
+        all_vehicle_data_except_this_cav = {}
+        for i in range(0, len(vehicles)):
+            vehicle = vehicles[i]
+            if vehicle is not self.__vehicle:
+                vehicle_color_rgb = vehicle.attributes.get("color")
+                if vehicle_color_rgb is None:
+                    vehicle_color_rgb = "None"
+
+                vehicle_transform = vehicle.get_transform()
+                vehicle_location = vehicle_transform.location
+                vehicle_rotation = vehicle_transform.rotation
+
+                vehicle_bounding_box = vehicle.bounding_box
+                vehicle_center = vehicle_bounding_box.location
+                vehicle_extent = vehicle_bounding_box.extent
+
+                vehicle_speed_l2_norm = Cav.speed_l2_norm(vehicle.get_velocity())
+
+                vehicle_data = {
+                    "bp_id": vehicle.type_id,
+                    "color": vehicle_color_rgb,
+                    "location": {
+                        "x": vehicle_location.x,
+                        "y": vehicle_location.y,
+                        "z": vehicle_location.z
+                    },
+                    "center": {
+                        "x": vehicle_center.x,
+                        "y": vehicle_center.y,
+                        "z": vehicle_center.z
+                    },
+                    "angle": {
+                        "pitch": vehicle_rotation.pitch,
+                        "roll": vehicle_rotation.roll,
+                        "yaw": vehicle_rotation.yaw
+                    },
+                    "extent": {
+                        "x": vehicle_extent.x,
+                        "y": vehicle_extent.y,
+                        "z": vehicle_extent.z
+                    },
+                    "speed": vehicle_speed_l2_norm
+                }
+                all_vehicle_data_except_this_cav[vehicle.id] = vehicle_data
+
+        data = {
+            "actor": f"cav_{self.__index}",
+            "frame": frame_id,
+            "sensors": {
+                "cameras": all_camera_data,
+                "vehicle_speed": {
+                    "speed": {
+                        "x": cav_speed.x,
+                        "y": cav_speed.y,
+                        "z": cav_speed.z
+                    }
+                },
+                "vehicle_pose": {
+                    "location": {
+                        "x": cav_location.x,
+                        "y": cav_location.y,
+                        "z": cav_location.z
+                    },
+                    "rotation": {
+                        "pitch": cav_rotation.pitch,
+                        "roll": cav_rotation.roll,
+                        "yaw": cav_rotation.yaw
+                    }
+                },
+                "lidar_pose": {
+                    "location": {
+                        "x": lidar_location.x,
+                        "y": lidar_location.y,
+                        "z": lidar_location.z
+                    },
+                    "rotation": {
+                        "pitch": lidar_rotation.pitch,
+                        "roll": lidar_rotation.roll,
+                        "yaw": lidar_rotation.yaw
+                    }
+                },
+                "imu_measurement": {
+                    "accelerometer": {
+                        "x": accelerometer.x,
+                        "y": accelerometer.y,
+                        "z": accelerometer.z
+                    },
+                    "gyroscope": {
+                        "x": gyroscope.x,
+                        "y": gyroscope.y,
+                        "z": gyroscope.z
+                    },
+                    "compass": compass
+                }
+            },
+            "vehicles": all_vehicle_data_except_this_cav
+        }
+
+        yaml_file_path = self.__save_dir_path + f"/{frame_id}.yaml"
+        with open(yaml_file_path, "w", encoding="utf-8") as f:
+            yaml.dump(data, f, default_flow_style=False, sort_keys=False, allow_unicode=True)
+
+    @staticmethod
+    def speed_l2_norm(speed):
+        return math.sqrt(speed.x ** 2 + speed.y ** 2 + speed.z ** 2)
