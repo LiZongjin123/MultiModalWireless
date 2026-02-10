d9bef6bea7706f95d9ed95686a41cda656d394cd
weqweqe647 <2575634450@qq.com>
2026-02-10 13:50:22 +0800

    finished: radar_data_saving

---
+++ blender_script.py
@@ -0,0 +1,5 @@
+# import bpy
+#
+#
+# class BlenderScript:
+#     def __init__(self, scene_dir):
--- rsu.py
+++ rsu.py
@@ -1,3 +1,5 @@
+import json
+
 import carla
 from mountable import Mountable
 
@@ -17,6 +19,7 @@ class Rsu(Mountable):
         self._camera_saving()
         self._depth_camera_saving()
         self._lidar_saving()
+        self._radar_saving()
         self._yaml_data_saving(actors)
 
     def _camera_saving(self):
@@ -26,6 +29,22 @@ class Rsu(Mountable):
     def _depth_camera_saving(self):
         image = self._sensor_queues["depth_camera"].get()
         image.save_to_disk(f"{self._save_dir_path}/{image.frame}_depth_camera.png", carla.ColorConverter.LogarithmicDepth)
+
+    def _radar_saving(self):
+        radar_measurement = self._sensor_queues["radar"].get()
+        json_data_of_radar_measurement = []
+        for detection in radar_measurement:
+            json_data_of_detection = {
+                "velocity": detection.velocity,
+                "azimuth": detection.azimuth,
+                "altitude": detection.altitude,
+                "depth": detection.depth
+            }
+            json_data_of_radar_measurement.append(json_data_of_detection)
+
+        frame_id = radar_measurement.frame
+        with open(f"{self._save_dir_path}/{frame_id}.json", 'w', encoding='utf-8') as f:
+            json.dump(json_data_of_radar_measurement, f, ensure_ascii=False, indent=4)
 
     def _generate_yaml_data_of_sensors(self):
         yaml_data_of_sensor_poses = self._generate_yaml_data_of_all_sensor_poses()
